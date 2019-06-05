29eba11fdb4d3d618451ed3c429abe25e54043a1
UtopiaMint <admin@lkp111138.me>
2019-06-05 22:52:25 +0800

    Formatting for /help

--- src/main/java/me/lkp111138/deebot/commands/HelpCommand.java
+++ src/main/java/me/lkp111138/deebot/commands/HelpCommand.java
@@ -2,6 +2,7 @@ package me.lkp111138.deebot.commands;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import me.lkp111138.deebot.translation.Translation;
 
@@ -9,6 +10,7 @@ public class HelpCommand implements Command {
     @Override
     public void respond(TelegramBot bot, Message msg, String[] args) {
         int sender = msg.from().id();
-        bot.execute(new SendMessage(sender, Translation.get(null).HELP()));
+        SendMessage req = new SendMessage(sender, Translation.get(null).HELP()).parseMode(ParseMode.Markdown);
+        bot.execute(req);
     }
 }
