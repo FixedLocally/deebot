e3e309b3de2d63b10a1da251ec581b5143fa132d
UtopiaMint <admin@lkp111138.me>
2019-08-25 21:07:21 +0800

    Added more info to /runinfo

--- src/main/java/me/lkp111138/deebot/commands/RunInfoCommand.java
+++ src/main/java/me/lkp111138/deebot/commands/RunInfoCommand.java
@@ -2,14 +2,25 @@ package me.lkp111138.deebot.commands;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.request.SendMessage;
+import me.lkp111138.deebot.Main;
 import me.lkp111138.deebot.game.Game;
 
 public class RunInfoCommand implements Command {
     @Override
     public void respond(TelegramBot bot, Message msg, String[] args) {
         Game.RunInfo runInfo = Game.runInfo();
-        String text = String.format("Total games: %d\nRunning games: %d\nPlayers: %d", runInfo.gameCount, runInfo.runningCount, runInfo.playerCount);
-        bot.execute(new SendMessage(msg.chat().id(), text));
+        StringBuilder text = new StringBuilder(String.format("Total games: %d\nRunning games: %d\nPlayers: %d\nGames:", runInfo.gameCount, runInfo.runningCount, runInfo.playerCount));
+        if (msg.from().id() == Main.BOT_OWNER) {
+            for (Long key : runInfo.games.keySet()) {
+                Game game = runInfo.games.get(key);
+                text.append(String.format("Group ID: %s\nRunning: %s\nPlayers:\n", key, game.started()));
+                for (User player : game.getPlayers()) {
+                    text.append(String.format("<a href=\"tg://user?id=%s\">%s</a> (%s)", player.id(), player.firstName(), player.id()));
+                }
+            }
+        }
+        bot.execute(new SendMessage(msg.chat().id(), text.toString()));
     }
 }
