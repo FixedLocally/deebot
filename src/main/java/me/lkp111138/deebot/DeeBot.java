40d4162c0d93a88601d2ec7d3765ce29de1db35e
UtopiaMint <admin@lkp111138.me>
2019-10-16 12:58:41 +0800

    Added ban function

--- src/main/java/me/lkp111138/deebot/DeeBot.java
+++ src/main/java/me/lkp111138/deebot/DeeBot.java
@@ -29,64 +29,29 @@ public class DeeBot {
         init();
     }
 
-    private void init() {
-        this.bot.setUpdatesListener(list -> {
-            for (Update update : list) {
-                processUpdate(update);
+    public static boolean executeBan(long tgid, String type, int length, String reason) {
+        if (length < 0) {
+            return executeUnban(tgid);
+        }
+        try (Connection conn = Main.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("INSERT into bans (tgid, until, count, type, reason) VALUES (?, ?, ?, ?, ?)");
+            int expiry = (int) (System.currentTimeMillis() / 1000) + length;
+            stmt.setLong(1, tgid);
+            stmt.setInt(2, expiry);
+            stmt.setInt(3, 0);
+            stmt.setString(4, type);
+            stmt.setString(5, reason);
+            stmt.execute();
+            stmt.close();
+            Ban ban = new Ban(tgid, expiry, type);
+            Ban oldBan = bans.get(tgid);
+            if (oldBan == null || oldBan.expiry < ban.expiry) {
+                bans.put(tgid, ban);
             }
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-
-        // commands
-        commands.put("start", new StartCommand());
-        commands.put("play", new PlayCommand());
-        commands.put("startgame", commands.get("play"));
-        commands.put("join", new JoinCommand());
-        commands.put("flee", new FleeCommand());
-        commands.put("stats", new StatCommand());
-        commands.put("killgame", new KillGameCommand());
-        commands.put("extend", new ExtendCommand());
-        commands.put("config", new ConfigCommand());
-        commands.put("maintmode", new MaintModeCommand());
-        commands.put("runinfo", new RunInfoCommand());
-        commands.put("setlang", new SetLangCommand());
-        commands.put("help", new HelpCommand());
-        commands.put("feedback", new FeedbackCommand());
-        commands.put("achv", new AchvCommand());
-        commands.put("toggle69", new Toggle69Command());
-        commands.put("forcestart", new ForceStartCommand());
-        commands.put("fs", commands.get("forcestart"));
-        commands.put("donate", new DonateCommand());
-        commands.put("nextgame", new NextGameCommand());
-
-        // achievements
-        Achievement.registerAchievement(new FirstGameAchievement());
-        Achievement.registerAchievement(new FirstWinAchievement());
-        Achievement.registerAchievement(new PlayWithMintAchievement());
-        Achievement.registerAchievement(new FirstBloodAchievement());
-        Achievement.registerAchievement(new RookieAchievement());
-        Achievement.registerAchievement(new FamiliarizedAchievement());
-        Achievement.registerAchievement(new AddictedAchievement());
-        Achievement.registerAchievement(new AmateurAchievement());
-        Achievement.registerAchievement(new AdeptAchievement());
-        Achievement.registerAchievement(new ExpertAchievement());
-        Achievement.registerAchievement(new LoseItAllAchievement());
-        Achievement.registerAchievement(new DeepFriedAchievement());
-
-        // bans
-        try (PreparedStatement stmt = Main.getConnection().prepareStatement("SELECT tgid, until, type from bans WHERE until>?")) {
-            stmt.setInt(1, (int) (System.currentTimeMillis() / 1000));
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                long tgid = rs.getLong(1);
-                Ban oldBan = bans.get(tgid);
-                Ban ban = new Ban(rs.getLong(1), rs.getInt(2), rs.getString(3));
-                if (oldBan == null || oldBan.expiry < ban.expiry) {
-                    bans.put(tgid, ban);
-                }
-            }
+            return true;
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
     }
 
@@ -216,21 +181,77 @@ public class DeeBot {
         return ban.type;
     }
 
-    public static void executeBan(long tgid, String type, int length, String reason) {
+    private static boolean executeUnban(long tgid) {
         try (Connection conn = Main.getConnection()) {
-            PreparedStatement stmt = conn.prepareStatement("INSERT into bans (tgid, until, count, type, reason) VALUES (?, ?, ?, ?, ?)");
-            int expiry = (int) (System.currentTimeMillis() / 1000) + length;
+            PreparedStatement stmt = conn.prepareStatement("update bans set until=unix_timestamp() where until>unix_timestamp() and tgid=?");
             stmt.setLong(1, tgid);
-            stmt.setInt(2, expiry);
-            stmt.setInt(3, 0);
-            stmt.setString(4, type);
-            stmt.setString(5, reason);
             stmt.execute();
             stmt.close();
-            Ban ban = new Ban(tgid, expiry, type);
-            Ban oldBan = bans.get(tgid);
-            if (oldBan != null && oldBan.expiry < ban.expiry) {
-                bans.put(tgid, ban);
+            bans.remove(tgid);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void init() {
+        this.bot.setUpdatesListener(list -> {
+            for (Update update : list) {
+                processUpdate(update);
+            }
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+
+        // commands
+        commands.put("start", new StartCommand());
+        commands.put("play", new PlayCommand());
+        commands.put("startgame", commands.get("play"));
+        commands.put("join", new JoinCommand());
+        commands.put("flee", new FleeCommand());
+        commands.put("stats", new StatCommand());
+        commands.put("killgame", new KillGameCommand());
+        commands.put("extend", new ExtendCommand());
+        commands.put("config", new ConfigCommand());
+        commands.put("maintmode", new MaintModeCommand());
+        commands.put("runinfo", new RunInfoCommand());
+        commands.put("setlang", new SetLangCommand());
+        commands.put("help", new HelpCommand());
+        commands.put("feedback", new FeedbackCommand());
+        commands.put("achv", new AchvCommand());
+        commands.put("toggle69", new Toggle69Command());
+        commands.put("forcestart", new ForceStartCommand());
+        commands.put("fs", commands.get("forcestart"));
+        commands.put("donate", new DonateCommand());
+        commands.put("nextgame", new NextGameCommand());
+        commands.put("bana", new BanCommand("ADMIN"));
+        commands.put("banc", new BanCommand("COMMAND"));
+
+        // achievements
+        Achievement.registerAchievement(new FirstGameAchievement());
+        Achievement.registerAchievement(new FirstWinAchievement());
+        Achievement.registerAchievement(new PlayWithMintAchievement());
+        Achievement.registerAchievement(new FirstBloodAchievement());
+        Achievement.registerAchievement(new RookieAchievement());
+        Achievement.registerAchievement(new FamiliarizedAchievement());
+        Achievement.registerAchievement(new AddictedAchievement());
+        Achievement.registerAchievement(new AmateurAchievement());
+        Achievement.registerAchievement(new AdeptAchievement());
+        Achievement.registerAchievement(new ExpertAchievement());
+        Achievement.registerAchievement(new LoseItAllAchievement());
+        Achievement.registerAchievement(new DeepFriedAchievement());
+
+        // bans
+        try (PreparedStatement stmt = Main.getConnection().prepareStatement("SELECT tgid, until, type from bans WHERE until>?")) {
+            stmt.setInt(1, (int) (System.currentTimeMillis() / 1000));
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                long tgid = rs.getLong(1);
+                Ban oldBan = bans.get(tgid);
+                Ban ban = new Ban(rs.getLong(1), rs.getInt(2), rs.getString(3));
+                if (oldBan == null || oldBan.expiry < ban.expiry) {
+                    bans.put(tgid, ban);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
