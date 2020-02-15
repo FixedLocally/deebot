493bf837864bf0d594e6b78029d95f5968d048b9
UtopiaMint <admin@lkp111138.me>
2020-02-15 16:00:58 +0800

    Fixed another NPE

--- src/main/java/me/lkp111138/deebot/game/Game.java
+++ src/main/java/me/lkp111138/deebot/game/Game.java
@@ -906,7 +906,7 @@ public class Game {
 
     private <T extends BaseRequest<T, R>, R extends BaseResponse> void execute(T request, Callback<T, R> callback, int failCount) {
         Map<String, Object> params = request.getParameters();
-        boolean isBroadcast = params.get("chat_id").toString().equals(String.valueOf(gid));
+        boolean isBroadcast = params.getOrDefault("chat_id", "").toString().equals(String.valueOf(gid));
         boolean willThrottle = request instanceof SendMessage && params.get("reply_markup") == null && isBroadcast;
         if (request instanceof SendMessage) {
             ((SendMessage) request).parseMode(ParseMode.HTML).disableWebPagePreview(true);
