d20afae056e1d2d9f593cc9945ba34b2c8652b20
UtopiaMint <admin@lkp111138.me>
2020-02-15 16:05:45 +0800

    Fixed a typo

--- src/main/java/me/lkp111138/deebot/commands/TermsCommand.java
+++ src/main/java/me/lkp111138/deebot/commands/TermsCommand.java
@@ -10,7 +10,7 @@ public class TermsCommand implements Command {
             "1. By using @jokebig2bot (referred by \"the bot\" below), you agree to our terms.\n" +
             "2. You agree to not take any actions to gain chips unfairly, including but not limited to repeatedly playing with other self-controlled accounts.\n" +
             "3. You agree to not take any actions that may or will damage the integrity or efficiency of the systems that host the bot, including but not limit to spamming commands and compromising the bot's servers.\n" +
-            "4. You further agree that you will not take any actions that may or will damage other players' interests within the bot.n\n" +
+            "4. You further agree that you will not take any actions that may or will damage other players' interests within the bot.\n" +
             "5. Being able to use this bot is a privilege, not a right and we reserve the right to block any user or group from accessing this bot for any reason. The reason behind this is that you do not pay for server costs and spend time to do maintenance so I don't need troubles from you.\n" +
             "6. The terms can be updated from time to time with or without prior notice, however they will be accessible by /terms\n" +
             "7. If there's any discrepancies between different versions of the terms, the English version shall prevail.\n";
